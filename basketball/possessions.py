ab58f9090909c60fbb32749f36ae8dcb3f5a4297
Noah Kasmanoff <nkasmanoff@gmail.com>
2019-06-13 11:32:58 -0400

    updated bus model, trying to spot check possessions now.

---
+++ basketball/possessions.py
@@ -0,0 +1,260 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 13 10:53:59 2019
+
+@author: noahkasmanoff
+
+
+
+Possession Flagger. 
+
+Use this file to expansively study how a posssession changes, who starts with it, and compare it to a game that already
+happened. 
+
+
+"""
+
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
+
+#%% Utility functions. 
+
+
+def substitution_correction(subs,lineup):
+    """
+    
+    Go through a game dataframe. Correct and confirm that the
+    substitution team assignments are correct.
+
+    Parameters
+    ----------
+
+    subs : df
+        pandas dataframe of every substitution to occur during a game.
+
+    lineups : df
+        provided data, the lineup for that given game
+
+    Returns
+    -------
+
+    subs : df
+        Corrected version of subs where the
+        team assignments for the player are correct.
+
+    """
+    
+    #Step 1 of cleaning, delete extra cols and make a person1 col to merge w/ subs.
+    lineup['Person1'] = lineup['Person_id']    
+    
+    del lineup['Period'],lineup['status'],lineup['Game_id']
+    del subs['Team_id']
+    
+    first = pd.merge(subs, lineup,
+        on = ['Person1'], how = 'left')
+    first.drop_duplicates(inplace=True)
+    del lineup['Person1']
+    
+    
+    weird = first[['Person2','Team_id']]  #now act in parralel. make a df of "first" with only nan cols.
+    #and weird has every col except but just p2 and team.
+    nanmask = first['Team_id'].isna().values
+    weird = weird[~nanmask]
+    nanfirst = first[nanmask]
+    nanfirst.dropna(axis=1,inplace=True) #remove nan columns to be added back on
+    weird['Person1'] = weird['Person2']
+    weird = weird[['Person1','Team_id']]
+    
+    second = pd.merge(nanfirst, weird,
+        on = ['Person1'], how = 'left')
+    del first['Person_id']
+    second.drop_duplicates(inplace=True) #remove extraneious cols and shove this into the first[nanmask]!
+    if len(second.values) >0:
+        first[nanmask] = second.values #and that's it.
+
+    subs = first[['Game_id','Event_Num','Event_Msg_Type','Period','WC_Time','PC_Time','Action_Type','Option1','Option2',
+                'Option3','Team_id','Person1','Person2','Team_id_type','Event_Msg_Type_Description']]
+
+    corrected_subs = subs
+    return corrected_subs
+
+
+
+def freethrowexceptions(z):
+    """Apply statement to encode end of a possession for free throws. 
+    The tricky case here is confirm this is the final free throw attempt, and if it did not go in. 
+    
+    In other cases the other possession change flags will capture the end of the possession, 
+    but this does.
+    
+    """
+    
+    if z['Event_Msg_Type'] == 3:
+        if z['Action_Type_Description'].split(' ')[-1] == z['Action_Type_Description'].split(' ')[-3]: #if final free throw
+                
+            if z['Option1'] == 1 and z['Action_Type_Description'].split(' ')[-1] != '1':
+                    #made final free throw. 
+                    return True
+      
+    
+    return False
+
+
+def possession_flagger(pbp_singlegame):
+    """
+    
+    Flags the end of a possession as designated by the rules for this question, and creates a counter similar
+    to the one for points scored of each team, denoting the total # of possessions that team has 
+    throughout the game. 
+    
+    Parameters
+    ----------
+    
+    pbp_singlegame : dataframe
+        Pandas dataframe of the play by play of the game, already slightly modified. 
+        
+    Returns
+    -------
+    
+    pbp_singlegame : dataframe
+        Pandas dataframe of the play by play of the game, even more modified such that the possesions of each team is now
+        attached. 
+    
+    
+    """
+    #first, sort according to event num, have to do this for possessions, but not other stuff. 
+    pbp_singlegame.sort_values(['Event_Num'],
+        ascending=[True],inplace=True)
+        
+    
+    #create new column for each type of possession change, combine at the end. 
+    pbp_singlegame['Poss Change 1'] = pbp_singlegame['Event_Msg_Type'].apply(lambda z: True if z == 1 else False)
+    pbp_singlegame['Poss Change Rebound'] = ((pbp_singlegame['Team_id'] != pbp_singlegame['Team_id'].shift(-1)) & (pbp_singlegame['Event_Msg_Type_Description'].str.contains('Rebound')))   
+    pbp_singlegame['Poss Change 5'] = pbp_singlegame['Event_Msg_Type'].apply(lambda z: True if z == 5 else False)
+    pbp_singlegame['Poss Change 13'] = pbp_singlegame['Event_Msg_Type'].apply(lambda z: True if z == 13 else False)
+    pbp_singlegame['Poss Change 6'] = pbp_singlegame.apply(lambda z: freethrowexceptions(z),axis=1)
+    
+    #merge all those possession change types together. 
+    pbp_singlegame['Poss Change'] = pbp_singlegame[pbp_singlegame.columns[-5:]].sum(axis=1)
+    pbp_singlegame.drop(columns = pbp_singlegame.columns[-6:-1],inplace=True)
+   # return pbp_singlegame
+    pbp_singlegame.sort_values(['Period','PC_Time','Option1','WC_Time','Event_Num'],
+            ascending=[True,False,True,True,True],inplace=True)
+    
+    pbp_singlegame['npossessions'] = pbp_singlegame.groupby('Team_id', axis = 0,sort=False)['Poss Change'].cumsum()
+
+    team1npossessions = pbp_singlegame.loc[pbp_singlegame.iloc[:,10] == teams[0]]
+    team2npossessions = pbp_singlegame.loc[pbp_singlegame.iloc[:,10] == teams[1]]
+    del pbp_singlegame['npossessions'] #this is because it copies, don't do this. 
+    
+    pbp_singlegame = pd.merge(pbp_singlegame, team1npossessions.loc[:,['Event_Num','npossessions']], on = 'Event_Num', how = 'left')
+    pbp_singlegame = pd.merge(pbp_singlegame, team2npossessions.loc[:,['Event_Num','npossessions']], on = 'Event_Num', how = 'left')
+
+    # fill forward for NaNs
+    pbp_singlegame.loc[:,['npossessions_x', 'npossessions_y']] = pbp_singlegame.loc[:,['npossessions_x', 'npossessions_y']].fillna(method = 'ffill')
+    pbp_singlegame.loc[:,['npossessions_x', 'npossessions_y']] = pbp_singlegame.loc[:,['npossessions_x', 'npossessions_y']].fillna(0) # for the start of the game
+    return pbp_singlegame
+
+
+def score_aggregator(pbp_singlegame):
+    """
+    
+    Aggregates the score of the game. 
+
+    Parameters
+    ----------
+    
+    pbp_singlegame : dataframe
+        Pandas dataframe of the play by play of the game. 
+        
+    Returns
+    -------
+    
+    pbp_singlegame : dataframe
+        Pandas dataframe of the play by play of the game, even more modified such that the score of each team is now
+        attached. 
+    
+        
+    """
+    
+    #first, make all non-scoring plays have their option 1 value equal 0. This is the value corresponding to made points anyway. 
+    
+    pbp_singlegame.loc[(pbp_singlegame['Event_Msg_Type'] == 3)&(pbp_singlegame['Option1'] != 1), 'Option1'] = 0
+    pbp_singlegame.loc[pbp_singlegame['Event_Msg_Type'] == 2, 'Option1'] = 0
+    pbp_singlegame.loc[pbp_singlegame['Event_Msg_Type'] == 5, 'Option1'] = 0
+    pbp_singlegame.loc[pbp_singlegame['Event_Msg_Type'] == 6, 'Option1'] = 0
+    
+    pbp_singlegame['score'] = pbp_singlegame.groupby('Team_id', axis = 0,sort=False)['Option1'].cumsum()
+    team1score = pbp_singlegame.loc[pbp_singlegame.iloc[:,10] == teams[0]]
+    team2score = pbp_singlegame.loc[pbp_singlegame.iloc[:,10] == teams[1]]
+    del pbp_singlegame['score'] #this is because it copies, don't do this. 
+    
+    pbp_singlegame = pd.merge(pbp_singlegame, team1score.loc[:,['Event_Num','score']], on = 'Event_Num', how = 'left')
+    pbp_singlegame = pd.merge(pbp_singlegame, team2score.loc[:,['Event_Num','score']], on = 'Event_Num', how = 'left')
+
+    # fill forward for NaNs
+    pbp_singlegame.loc[:,['score_x', 'score_y']] = pbp_singlegame.loc[:,['score_x', 'score_y']].fillna(method = 'ffill')
+    pbp_singlegame.loc[:,['score_x', 'score_y']] = pbp_singlegame.loc[:,['score_x', 'score_y']].fillna(0) # for the start of the game
+    
+    return pbp_singlegame
+
+
+
+#%%
+
+#load in data. 
+pbp = pd.read_csv('Basketball Analytics/Play_by_Play.txt',delimiter='\t')
+lineup = pd.read_csv('Basketball Analytics/Game_Lineup.txt',delimiter='\t')
+codes = pd.read_csv('Basketball Analytics/Event_Codes.txt',delimiter = '\t')
+
+sample_games = pbp['Game_id'].unique()
+
+for game_num in range(len(sample_games))[11:12]:
+    pbp = pd.read_csv('Basketball Analytics/Play_by_Play.txt',delimiter='\t')
+    lineup = pd.read_csv('Basketball Analytics/Game_Lineup.txt',delimiter='\t')
+    codes = pd.read_csv('Basketball Analytics/Event_Codes.txt',delimiter = '\t')
+
+    game = sample_games[game_num] # for now, not iterative.
+    print("game id: ", game)
+    print("game num: ",game_num)
+    teams = lineup.loc[lineup['Game_id'] == game]['Team_id'].unique() #locate the two teams in the game. 
+    
+    
+    #sort according to this, it may end up changing . 
+    pbp_singlegame = pbp.loc[pbp['Game_id'] == sample_games[game_num]] 
+    
+    #translate using codes
+    pbp_singlegame = pbp_singlegame.merge( codes,
+        on = ['Event_Msg_Type', 'Action_Type'], how = 'left')
+    
+    #obtain starting lineups
+    starting_lineup = lineup.loc[(lineup['Game_id'] == game)] #starting lineup of the game
+    
+    subsmask = pbp_singlegame['Event_Msg_Type'] == 8
+    subs = pbp_singlegame[subsmask] #dataframe of all substitutions. 
+    
+    
+    playersin = pd.DataFrame(starting_lineup.loc[(starting_lineup['Period'] == 1),['Team_id','Person_id']])
+    subs_correct = substitution_correction(subs,starting_lineup)
+    pbp_singlegame[subsmask] =  subs_correct #fixes substitutions, and cofnirms taem names are correct. 
+    subs = pbp_singlegame[subsmask] 
+    pbp_singlegame = pbp_singlegame.sort_values(['Period','PC_Time','WC_Time','Event_Num'],
+        ascending=[True,False,True,True])
+    
+    pbp_singlegame = possession_flagger(pbp_singlegame)
+    pbp_singlegame = score_aggregator(pbp_singlegame)
+    pbp_singlegame['Team_id_type'] = pbp_singlegame['Team_id_type'].apply(lambda z: 'MIN' if z == 3 else z)
+    pbp_singlegame['Team_id_type'] = pbp_singlegame['Team_id_type'].apply(lambda z: 'HOU' if z == 2 else z)
+#%%
+
+pbp_singlegame.columns
+
+pbp_viewer = pbp_singlegame[['Team_id_type','Event_Msg_Type_Description',
+       'Action_Type_Description','npossessions_x',
+       'npossessions_y', 'score_x', 'score_y']]
+
+pbp_viewer.rename(columns = {'npossessions_x':'MIN_nposs','score_x':'MIN_score',
+                             'npossessions_y':'HOU_nposs','score_y':'HOU_score'},inplace=True)
