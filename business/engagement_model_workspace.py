b8bba59b69556a7e1faf4a9b400b4e29b71e97d3
Noah Kasmanoff <nkasmanoff@gmail.com>
2019-07-03 11:08:54 -0400

    business MAPE ~2.6% :)

--- business/engagement_model_workspace.py
+++ business/engagement_model_workspace.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 
 from xgboost import XGBRegressor
+from sklearn.model_selection import train_test_split
 
 def mean_absolute_percentage_error(y_true, y_pred):
     import numpy as np
@@ -21,10 +22,42 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
+
+ks = [300,300,300] #every other
+#%%
+
+MAPES = []
+for k_prof in ks:
+    for k_hash in ks:
+        
+        X, y = process_data('Business Analytics/training_set.csv',k_prof = k_prof,k_hash = k_hash,training=True)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        model = XGBRegressor(colsample_bytree =0.5, gamma = 0.05, max_depth = 4, min_child_weight = 4, n_estimators = 1000, subsample = 0.6) 
+
+        model.fit(X_train, y_train)
+        print("MAPE Train Score ")
+        print(mean_absolute_percentage_error(y_train, model.predict(X_train)))
+    
+    
+        print("MAPE Test Score ")
+        print(mean_absolute_percentage_error(y_test, model.predict(X_test)))
+        MAPES.append(mean_absolute_percentage_error(y_test, model.predict(X_test)))
+    
+      #  print("Best parameters ")
+      #  print(grid.best_params_)
+        
+#%%
+print(np.mean(MAPES), '+/-', 2*np.std(MAPES))
+#%%
+
+
+#%%
 X, y = process_data('Business Analytics/training_set.csv',k_prof = 300,k_hash = 300,training=True)
 
+from xgboost import XGBRegressor
 
-# Initialize XGB
+
+# Initialize XGB and GridSearch
 model = XGBRegressor(colsample_bytree =0.5, gamma = 0.05, max_depth = 4, min_child_weight = 4, n_estimators = 1000, subsample = 0.6) 
 model.fit(X, y)
 
