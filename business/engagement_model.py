24e49ea18b445e43556733d9168593d40e4bb368
Noah Kasmanoff <nkasmanoff@gmail.com>
2019-07-11 15:38:41 -0400

    put in place some working means to generate a population of predictions for the holdout set, and then use the median value of that to submit.

--- business/engagement_model.py
+++ business/engagement_model.py
@@ -21,18 +21,29 @@ def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 
-X, y = process_data('Business Analytics/training_set.csv',k_prof = 300,k_hash = 300,training=True)
+model = XGBRegressor(colsample_bytree =0.5, gamma = 0.05, max_depth = 4, min_child_weight = 4, n_estimators = 1000, subsample = 0.6) 
 
 
 # Initialize XGB
-model = XGBRegressor(colsample_bytree =0.5, gamma = 0.05, max_depth = 4, min_child_weight = 4, n_estimators = 1000, subsample = 0.6) 
-model.fit(X, y)
+holdout_prediction_population = []
+prediction_run = 0
+while prediction_run  < 5:
+	X, y = process_data('Business Analytics/training_set.csv',k_prof = 300,k_hash = 300,training=True)
+	model.fit(X, y)
 
-holdout = pd.read_csv('Business Analytics/holdout_set.csv',encoding = 'unicode_escape')
+	holdout = pd.read_csv('Business Analytics/holdout_set.csv',encoding = 'unicode_escape')
+	X_holdout = process_data('Business Analytics/holdout_set.csv',k_prof = 300,k_hash = 300,training=False)
+	holdout_predictions = model.predict(X_holdout)
+	holdout_predictions = np.array([int(round(y)) for y in holdout_predictions])
+	holdout_prediction_population.append(holdout_predictions)
 
-X_holdout = process_data('Business Analytics/holdout_set.csv',k_prof = 300,k_hash = 300,training=False)
-holdout_predictions = model.predict(X_holdout)
-holdout_predictions = np.array([int(round(y)) for y in holdout_predictions])
+
+for i,_ in enumerate(holdout_predictions): #iterate over each test point
+	ith_values_predictions = holdout_prediction_population[:,i]
+	#get the median, and make that the new prediction 
 holdout['Engagements'] = holdout_predictions
 
-holdout.to_csv('holdout_set_TEAM_NAME.csv',index=False)
+
+
+
+#holdout.to_csv('holdout_set_TEAM_NAME.csv',index=False)
